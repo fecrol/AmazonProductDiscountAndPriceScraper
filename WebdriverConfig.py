5609506c23a2b15c1fcfc49a3da22aa6ae0351e2
Maciej Fec <maciejfec1996@gmail.com>
2022-08-06 20:28:49 +0100

    ADD headless mode to chrome driver

--- WebdriverConfig.py
+++ WebdriverConfig.py
@@ -8,10 +8,12 @@ class WebdriverConfig:
         
         self.__chrome_service = Service(executable_path=ChromeDriverManager().install())
         self.__options = webdriver.ChromeOptions()
+        self.__options.add_argument('--log-level=1')
+        self.__options.headless = True
         self.__driver = None
     
     def start_driver(self):
 
-        self.__driver = webdriver.Chrome(service=self.__chrome_service)
+        self.__driver = webdriver.Chrome(service=self.__chrome_service, options=self.__options)
 
         return self.__driver
