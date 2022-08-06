50be53edf214b5339d47eee468e32c60eae6d80c
Maciej Fec <maciejfec1996@gmail.com>
2022-08-06 16:41:18 +0100

    initial commit

---
+++ WebdriverConfig.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+class WebdriverConfig:
+
+    def __init__(self):
+        
+        self.__chrome_service = Service(executable_path=ChromeDriverManager().install())
+        self.__options = webdriver.ChromeOptions()
+        self.__driver = None
+    
+    def start_driver(self):
+
+        self.__driver = webdriver.Chrome(service=self.__chrome_service)
+
+        return self.__driver
