02a42db2c3d9421af10929516cc4db10dd75a4fb
Maciej Fec <maciejfec1996@gmail.com>
2022-08-06 19:56:37 +0100

    close the cookies window

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 from WebdriverConfig import WebdriverConfig
+from pages.CookiePreferences import CookiePreferences
 import time
 
 url = "https://www.amazon.co.uk/Logitech-Rechargeable-Multi-Device-Programmable-Productivity/dp/B071KZS3MF/ref=sr_1_5?crid=2D1EPUCESTOID&keywords=logitech+mx+master&qid=1659795024&sprefix=logitech+mx+master%2Caps%2C840&sr=8-5"
@@ -9,6 +10,9 @@ driver = webdriver_config.start_driver()
 
 driver.get(url)
 
-time.sleep(2)
+cookie_preferences_page = CookiePreferences(driver)
+cookie_preferences_page.click_accept_cookies_btn()
+
+time.sleep(30)
 
 driver.quit()
