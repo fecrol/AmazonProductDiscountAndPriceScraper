b9b739ccdc3315a834eeb18622f3e72774388732
Maciej Fec <maciejfec1996@gmail.com>
2022-08-07 17:24:55 +0100

    ADD methods to grab cli arguments

--- utilities/CliArgumentParser.py
+++ utilities/CliArgumentParser.py
@@ -3,12 +3,33 @@ import json
 
 class CliArgumentParser:
 
-    def get_args_as_dict(self):
-
+    def __init__(self):
+        
         try:
-            args_as_string = sys.argv[1].replace("'", "\"")
-            args_as_dict = json.loads(args_as_string)
-
-            return args_as_dict
+            self.__args_as_string = sys.argv[1].replace("'", "\"")
+            self.__args_as_dict = json.loads(self.__args_as_string)
         except:
             print("An error has occured")
+            sys.exit()
+    
+    def get_url(self):
+        """
+        Mandatory command line argument needed to run the script
+        """
+
+        try:
+            return self.__args_as_dict["url"]
+        except:
+            print("Error: No URL found in the list of arguments")
+            sys.exit()
+    
+    def get_csv_filename(self):
+        """
+        Mandatory command line argument needed to run the script
+        """
+
+        try:
+            return self.__args_as_dict["csv-filename"]
+        except:
+            print("Error: No CSV Filename found in list of arguments")
+            sys.exit()
