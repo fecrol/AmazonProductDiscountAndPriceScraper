cce3277fee43d5953c1d15637b9c3e5299adc833
Maciej Fec <maciejfec1996@gmail.com>
2022-08-07 16:38:21 +0100

    ADD method to parse for cli arguments (string in form of dictionary)

--- utilities/CliArgumentParser.py
+++ utilities/CliArgumentParser.py
@@ -1,5 +1,14 @@
+import sys
+import json
 
 class CliArgumentParser:
 
-    def __init__(self):
-        pass
+    def get_args_as_dict(self):
+
+        try:
+            args_as_string = sys.argv[1].replace("'", "\"")
+            args_as_dict = json.loads(args_as_string)
+
+            return args_as_dict
+        except:
+            print("An error has occured")
