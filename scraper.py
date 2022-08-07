11b0a66ffa8ff43d80adf4fb5f4885223779969d
Maciej Fec <maciejfec1996@gmail.com>
2022-08-07 13:18:20 +0100

    WRITE gathered data to csv file

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 from WebdriverConfig import WebdriverConfig
 from pages.CookiePreferences import CookiePreferences
 from pages.ProductPage import ProductPage
+from datetime import datetime
+
+from utilities.CsvWriter import CsvWriter
 
 url = "https://www.amazon.co.uk/Logitech-Rechargeable-Multi-Device-Programmable-Productivity/dp/B071KZS3MF/ref=sr_1_5?crid=2D1EPUCESTOID&keywords=logitech+mx+master&qid=1659795024&sprefix=logitech+mx+master%2Caps%2C840&sr=8-5"
 
@@ -19,5 +22,14 @@ savings_percentage = savings_percentage.replace("-", "").replace("%", "")
 
 price_to_pay = product_page.get_price_to_pay()
 price_to_pay = ".".join(price_to_pay.split("\n")).replace("£", "")
+
+date = datetime.today().strftime("%Y-%m-%d")
+
+csv_filename = "price_data.csv"
+data = [[date, savings_percentage, price_to_pay]]
+columns = ["Date", "Discount Percentage", "Current Price"]
+
+csv_writer = CsvWriter()
+csv_writer.append_to_csv(csv_filename, data, columns)
 
 driver.quit()
